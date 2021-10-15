9f4828e0e1d1405419206c993fc0d9211ad93345
Raúl Alonso <67586098+RaAlMer@users.noreply.github.com>
2021-10-15 08:11:15 +0200

    Create TributePage.cs

---
+++ TributePage.cs
@@ -0,0 +1,27 @@
+html {
+  background-color: hsla(0, 0%, 91%, 1);
+}
+#title {
+  text-align: center;
+}
+#img-div {
+  border: 40px solid transparent;
+  background-color: white;
+}
+h2, h3, figure, p {
+  text-align: center;
+}
+#image {
+  max-width: 100%;
+  height: auto;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+ul {
+  display: inline-block;
+  text-align: left;
+}
+#list {
+  text-align: center;
+}
